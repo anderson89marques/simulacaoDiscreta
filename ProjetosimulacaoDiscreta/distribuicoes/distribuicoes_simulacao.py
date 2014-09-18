a427f339bf62ca4e6bef825f9ccf2b06cd808b4f
Anderson Marques <andersonmarques@acceptiondev.com.br>
2014-09-18 11:39:30 -0300

    implementação binomial1, geométrica, fatorial, combinação

---
+++ ProjetosimulacaoDiscreta/distribuicoes/distribuicoes_simulacao.py
@@ -0,0 +1,31 @@
+__author__ = 'andersonmarques'
+
+class Distribuicao:
+    def combinacao(self, n, k):
+        return self.fat(n)/(self.fat(k)*self.fat(n-k))
+
+    def fat(self, n):
+        if n == 0 or n == 1:
+            return 1
+        else:
+            return n * self.fat(n-1)
+
+    def binomialA(self, n, p, k):
+        r = 1.0
+        q = 1-p
+        for x in range(n):
+            if x < k:
+                r *= p
+            else:
+                r *= q
+        return self.combinacao(n, k) * r
+
+    def geometrica(self, n, p):
+        r = 1.0
+        q = 1-p
+        for x in range(n):
+            if x < n - 1:
+                r *= q
+            else:
+                r *= p
+        return r
---
+++ ProjetosimulacaoDiscreta/lista2/questao01a.py
@@ -0,0 +1,6 @@
+__author__ = 'andersonmarques'
+from distribuicoes.distribuicoes_simulacao import Distribuicao
+
+distri = Distribuicao()
+prob = distri.binomialA(3, 0.6, 2)
+print("probabilidade: %.4f" % prob)
